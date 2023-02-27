be6043db4ef27e433c9c3723997b9540d715c5f2
qverkk <membersy@gmail.com>
2023-02-27 21:22:05 +0100

    plasma feature

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../optional/nvidia.nix
+      ../../features/desktop/plasma
     ];
 
   # Bootloader.
@@ -43,13 +44,6 @@
     LC_TELEPHONE = "pl_PL.UTF-8";
     LC_TIME = "pl_PL.UTF-8";
   };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
