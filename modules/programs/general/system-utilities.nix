c9b3b5d2035e0159409931c6c1245604aab88fcd
qverkk <membersy@gmail.com>
2023-06-08 14:57:12 +0200

    Add more system utilities

--- modules/programs/general/system-utilities.nix
+++ modules/programs/general/system-utilities.nix
@@ -1,5 +1,29 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     dua
   ];
+
+  programs = {
+    # ls alternative
+    exa = {
+      enable = true;
+      enableAliases = true;
+    };
+
+    # cat alternative
+    bat.enable = true;
+
+    # tldr alternative
+    tealdeer = {
+      enable = true;
+      settings = {
+        display = {
+          compact = true;
+        };
+        updates = {
+          auto_update = true;
+        };
+      };
+    };
+  };
 }
