7dbce835ec40a027bfff294d3233800f83718f28
qverkk <membersy@gmail.com>
2023-06-18 17:39:48 +0200

    Fix codeium

--- overlays/nvim/codeium/default.nix
+++ overlays/nvim/codeium/default.nix
@@ -1,9 +1,19 @@
-final: prev: {
-  codeium-vim = prev.vimPlugins.codeium-vim.overrideAttrs (old: {
-    patches =
-      (old.patches or [])
-      ++ [
-        ./patches/wrap-with-steam-run.patch
-      ];
-  });
+self: super: {
+  codeium-vim = super.vimUtils.buildVimPluginFrom2Nix {
+    pname = "codeium.vim";
+    version = "2023-06-15";
+
+    src = super.fetchFromGitHub {
+      owner = "Exafunction";
+      repo = "codeium.vim";
+      rev = "99714b06b85e79d9247066f7612e9cc55458bcf1";
+      sha256 = "0g629r1103dxjzpi906xswkp5vilhkgyjz66avvm6m5j64xs5f8w";
+    };
+
+    meta.homepage = "https://github.com/Exafunction/codeium.vim/";
+
+    patches = [
+      ./patches/wrap-with-steam-run.patch
+    ];
+  };
 }
