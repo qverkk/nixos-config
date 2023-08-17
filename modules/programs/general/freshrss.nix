b7faf0a3d581cf9dd8012cea9907d89a68417ea4
qverkk <membersy@gmail.com>
2023-08-17 18:55:12 +0200

    Add support for freshrss extensions

--- modules/programs/general/freshrss.nix
+++ modules/programs/general/freshrss.nix
@@ -1,6 +1,7 @@
-{ pkgs
-, config
-, ...
+{
+  pkgs,
+  config,
+  ...
 }: {
   services.freshrss = {
     enable = true;
@@ -9,6 +10,14 @@
     passwordFile = config.age.secrets.freshrss.path;
     dataDir = "/var/lib/freshrss";
     virtualHost = "freshrss";
+    package = pkgs.freshrss.overrideAttrs (old: {
+      overrideConfig = pkgs.writeText "constants.local.php" ''
+        <?php
+              define('DATA_PATH', getenv('FRESHRSS_DATA_PATH'));
+              define('THIRDPARTY_EXTENSIONS_PATH', getenv('FRESHRSS_DATA_PATH') . '/extensions');
+              define('EXTENSIONS_PATH', getenv('FRESHRSS_DATA_PATH') . '/extensions');
+      '';
+    });
   };
 
   networking.extraHosts = "127.0.0.3 freshrss.local";
