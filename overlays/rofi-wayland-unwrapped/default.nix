81cd444fee4a1a301155ba2de7f057f78957baf2
qverkk <membersy@gmail.com>
2023-10-05 22:08:49 +0200

    Update rofi

--- overlays/rofi-wayland-unwrapped/default.nix
+++ overlays/rofi-wayland-unwrapped/default.nix
@@ -1,6 +1,6 @@
 self: super: {
   rofi-wayland-unwrapped = super.rofi-wayland-unwrapped.overrideAttrs (old: rec {
-    version = "c6b4dfe";
+    version = "9d11a2b4a9095b3f0a092155f749900f7eb8e047";
 
     src = super.fetchFromGitHub {
       owner = "lbonn";
