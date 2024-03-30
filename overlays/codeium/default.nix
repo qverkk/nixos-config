994b328d367f24dd680119b1db7837f95e0b6045
qverkk <membersy@gmail.com>
2024-03-30 22:32:07 +0100

    Codeium updates

--- overlays/codeium/default.nix
+++ overlays/codeium/default.nix
@@ -6,11 +6,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "codeium-lsp";
-  version = "v1.4.5";
+  version = "v1.6.7";
 
   src = fetchurl {
     url = "https://github.com/Exafunction/codeium/releases/download/language-server-${version}/language_server_linux_x64";
-    sha256 = "sha256-zLfhEq6/0T4pj4WIj10bFN2mRcpMXRu7lgtjZ62P4nM=";
+    sha256 = "sha256-tnhfo84pUiiptPvyP0NKXkSiETedH7An1NAqbwI5Wvg=";
   };
 
   nativeBuildInputs = [
