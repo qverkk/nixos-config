ac8a42261265bb6bb38590d59d67e701e4d6275c
Marek Filipowicz <membersy@gmail.com>
2024-06-19 09:48:58 +0200

    Update typescript-tools.lua

--- modules/programs/development/neovim/config/nvim/lua/qverkk/typescript-tools.lua
+++ modules/programs/development/neovim/config/nvim/lua/qverkk/typescript-tools.lua
@@ -1,4 +1,9 @@
 require("typescript-tools").setup({
+	settings = {
+		tsserver_file_preferences = {
+			importModuleSpecifierPreference = "non-relative",
+		},
+	},
 	on_attach = function ()
 		typescript_mappings()
 	end
