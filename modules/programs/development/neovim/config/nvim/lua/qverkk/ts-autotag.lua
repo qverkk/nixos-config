5b3f6f32a7b0b80ae722db9447b5f937137778da
Marek Filipowicz <membersy@gmail.com>
2024-06-19 09:56:41 +0200

    Create ts-autotag.lua

---
+++ modules/programs/development/neovim/config/nvim/lua/qverkk/ts-autotag.lua
@@ -0,0 +1,19 @@
+require("nvim-ts-autotag").setup({
+	opts = {
+		-- Defaults
+		enable_close_on_slash = true,
+	},
+})
+
+local autotag_group = vim.api.nvim_create_augroup("autotag_group_custom", {})
+
+-- rename tags on paste also
+vim.api.nvim_create_autocmd({ "User" }, {
+	group = autotag_group,
+	pattern = { "PastePost" },
+	callback = function()
+		if require("nvim-ts-autotag.config.plugin").get_opts(vim.bo.filetype).enable_rename then
+			require("nvim-ts-autotag.internal").rename_tag()
+		end
+	end,
+})
