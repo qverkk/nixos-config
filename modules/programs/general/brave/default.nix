df48ffaa6f78931a8fad463b61f1e21c47b59ce5
qverkk <membersy@gmail.com>
2024-06-23 22:36:15 +0200

    Add brave extensions

--- modules/programs/general/brave/default.nix
+++ modules/programs/general/brave/default.nix
@@ -2,5 +2,15 @@
   programs.chromium = {
     enable = true;
     package = pkgs.brave;
+    extensions = [
+      {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} # ublock origin
+      {id = "oboonakemofpalcgghocfoadofidjkkk";} # keepassxc
+      {id = "ldgfbffkinooeloadekpmfoklnobpien";} # raindrop
+      {id = "dbepggeogbaibhgnhhndojpepiihcmeb";} # vimium
+      {id = "mdjildafknihdffpkfmmpnpoiajfjnjd";} # consent o matic
+      {id = "lckanjgmijmafbedllaakclkaicjfmnk";} # clear urls
+      {id = "ldpochfccmkkmhdbclfhpagapcfdljkj";} # decentraleyes
+      {id = "gbmgphmejlcoihgedabhgjdkcahacjlj";} # wallabag
+    ];
   };
 }
