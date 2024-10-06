c6fb4df840d186360d5588064d64032b0786396a
qverkk <membersy@gmail.com>
2024-10-06 23:27:37 +0200

    Update yogi to unstable nixpkgs

--- hosts/yogi/default.nix
+++ hosts/yogi/default.nix
@@ -120,7 +120,7 @@
   console.keyMap = "pl2";
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
 
