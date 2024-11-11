06ae9f4eb49cbd70ae2d3d9bf41aae2e5870e562
qverkk <membersy@gmail.com>
2024-11-11 11:21:03 +0100

    Updates to support archlinux a bit

--- modules/programs/development/neovim/lazy-init.lua
+++ modules/programs/development/neovim/lazy-init.lua
@@ -115,7 +115,44 @@ require("lazy").setup({
 	"nvim-neotest/neotest-vim-test",
 	{ "folke/neodev.nvim", opts = {} },
 	"windwp/nvim-autopairs",
-	"windwp/nvim-ts-autotag"
+	"windwp/nvim-ts-autotag",
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		lazy = false,
+		config = function()
+			require("refactoring").setup()
+		end,
+	},
+	{
+		"rcasia/neotest-java",
+		ft = "java",
+		dependencies = {
+			"mfussenegger/nvim-jdtls",
+			"mfussenegger/nvim-dap", -- for the debugger
+			"rcarriga/nvim-dap-ui", -- recommended
+			"theHamsta/nvim-dap-virtual-text", -- recommended
+		},
+	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			adapters = {
+				["neotest-java"] = {
+					-- config here
+				},
+			},
+		},
+	},
 })
 
 require("qverkk")
