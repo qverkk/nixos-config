a7b4b11b16db1b10d707d3eecda8dbf9cb75e7bf
qverkk <membersy@gmail.com>
2024-11-15 17:52:09 +0100

    Fix: zellij config migration

--- modules/programs/development/zellij/home.nix
+++ modules/programs/development/zellij/home.nix
@@ -2,12 +2,12 @@
 {
   home.file.zellij-default-layout = {
     target = ".config/zellij/layout_default.kdl";
-    text = ${builtins.readFile ./layout_default.kdl};
+    text = ''${builtins.readFile ./layout_default.kdl}'';
   };
 
   home.file.zellij = {
     target = ".config/zellij/config.kdl";
-    text =       ${builtins.readFile ./config.kdl}
+    text = ''${builtins.readFile ./config.kdl}'';
   };
   programs.zellij = {
     enable = true;
