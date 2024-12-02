096e67b2cbc34881b3d9dce0f38af2214fcadf94
qverkk <membersy@gmail.com>
2024-12-02 19:29:14 +0100

    Add caskaydia code font

--- modules/programs/general/home/font/home.nix
+++ modules/programs/general/home/font/home.nix
@@ -1,19 +1,10 @@
 { pkgs, ... }:
-let
-  # nerdFonts = pkgs.nerd-fonts.override {
-  #   fonts = [
-  #     "JetBrainsMono"
-  #     "NerdFontsSymbolsOnly"
-  #     "CascadiaCode"
-  #   ];
-  # };
-in
 {
   fonts.fontconfig.enable = true;
   home.packages = [
-	pkgs.nerd-fonts.jetbrains-mono
-	# pkgs.nerd-fonts.cascadia-code
-	pkgs.nerd-fonts.symbols-only
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.nerd-fonts.caskaydia-cove
+    pkgs.nerd-fonts.symbols-only
     pkgs.jetbrains-mono
     pkgs.cascadia-code
   ];
