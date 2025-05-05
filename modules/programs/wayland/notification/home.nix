f48bd72ef6c4968d99297c95f87d41f5210318c5
qverkk <membersy@gmail.com>
2025-05-05 17:23:22 +0200

    Update packages

--- modules/programs/wayland/notification/home.nix
+++ modules/programs/wayland/notification/home.nix
@@ -3,13 +3,15 @@
   services.mako = {
     enable = true;
     #iconPath = "${pkgs.papirus-icon-theme.package}/share/icons/Papirus-Dark";
-    font = "CaskaydiaCove Nerd Font 12";
-    padding = "10,20";
-    anchor = "top-center";
-    width = 400;
-    height = 150;
-    borderSize = 2;
-    defaultTimeout = 12000;
+    settings = {
+      defaultTimeout = 12000;
+      borderSize = 2;
+      padding = "10,20";
+      width = 400;
+      height = 150;
+      font = "CaskaydiaCove Nerd Font 12";
+      anchor = "top-center";
+    };
     #backgroundColor = "#${colors.base00}dd";
     #borderColor = "#${colors.base03}dd";
     #textColor = "#${colors.base05}dd";
