e97da0928e082a30f06ab69c66c9592aeb13e235
qverkk <membersy@gmail.com>
2025-08-13 17:58:35 +0200

    Fix regreet? - FML WHY

--- modules/programs/wayland/greetd/default.nix
+++ modules/programs/wayland/greetd/default.nix
@@ -1,7 +1,8 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.regreet = {
     enable = true;
+	package = pkgs.regreet;
     settings = fromTOML ''
       [GTK]
       application_prefer_dark_theme = true
