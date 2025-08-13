b7c7a623634aab4a1c01f89d0defb08365958590
qverkk <membersy@gmail.com>
2025-08-13 18:15:27 +0200

    Switch to sddm instead of regreet

--- modules/programs/wayland/greetd/default.nix
+++ modules/programs/wayland/greetd/default.nix
@@ -1,11 +1,18 @@
 { pkgs, ... }:
 {
-  programs.regreet = {
-    enable = true;
-	package = pkgs.regreet;
-    settings = fromTOML ''
-      [GTK]
-      application_prefer_dark_theme = true
-    '';
-  };
+ #  programs.regreet = {
+ #    enable = true;
+	# package = pkgs.regreet;
+ #    # settings = fromTOML ''
+ #    #   [GTK]
+ #    #   application_prefer_dark_theme = true
+ #    # '';
+ #  };
+
+
+	services.displayManager.sddm = {
+		enable = true;
+		wayland.enable = true;
+	};
+
 }
