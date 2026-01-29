bb0cbdfdda826c6dd6de9e27db6f226f4f378937
qverkk <membersy@gmail.com>
2026-01-29 23:14:37 +0100

    Configure opencode

--- modules/programs/development/opencode/home.nix
+++ modules/programs/development/opencode/home.nix
@@ -47,10 +47,70 @@ in
       formatter = languages.formatter;
       lsp = languages.lsp;
       provider = providers.config;
+      enabled_providers = [ "github-copilot" ];
     };
     "opencode/oh-my-opencode.json".text = builtins.toJSON {
       "$schema" =
         "https://raw.githubusercontent.com/code-yeongyu/oh-my-opencode/master/assets/oh-my-opencode.schema.json";
+      "google_auth" = false;
+      "agents" = {
+        "plan" = {
+          "model" = "github-copilot/claude-haiku-4.5";
+        };
+        "build" = {
+          "model" = "github-copilot/claude-sonnet-4.5";
+        };
+        "Planner-Sisyphus" = {
+          "model" = "github-copilot/gemini-3-flash-preview";
+        };
+        "Sisyphus" = {
+          "model" = "github-copilot/gpt-5.2";
+        };
+        "oracle" = {
+          "model" = "github-copilot/gpt-5.2";
+        };
+        "explore" = {
+          "model" = "github-copilot/gpt-5-mini";
+        };
+        "librarian" = {
+          "model" = "github-copilot/gemini-3-flash-preview";
+        };
+        "document-writer" = {
+          "model" = "github-copilot/gemini-3-flash-preview";
+        };
+        "multimodal-looker" = {
+          "model" = "github-copilot/gemini-3-flash-preview";
+        };
+        "frontend-ui-ux-engineer" = {
+          "model" = "github-copilot/gemini-3-pro-preview";
+        };
+      };
+      "categories" = {
+        "visual-engineering" = {
+          "model" = "google/gemini-3-pro-preview";
+        };
+        "ultrabrain" = {
+          "model" = "github-copilot/gpt-5.2-codex";
+          # "variant" = "xhigh";
+        };
+        "artistry" = {
+          "model" = "github-copilot/gemini-3-pro-preview";
+          # "variant" = "max";
+        };
+        "quick" = {
+          "model" = "github-copilot/claude-haiku-4-5";
+        };
+        "unspecified-low" = {
+          "model" = "github-copilot/claude-sonnet-4-5";
+        };
+        "unspecified-high" = {
+          "model" = "github-copilot/claude-opus-4-5";
+          # "variant" = "max";
+        };
+        "writing" = {
+          "model" = "github-copilot/gemini-3-flash-preview";
+        };
+      };
     };
     "opencode/skill".source = skills.skillsSource + "/skill";
   };
