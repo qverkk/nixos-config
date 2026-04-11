4ff8652f5de4c574dacbe1c0d4f4f2104f045d26
qverkk <membersy@gmail.com>
2026-04-11 19:08:34 +0200

    Update cursor

--- overlays/cursor/default.nix
+++ overlays/cursor/default.nix
@@ -57,7 +57,7 @@ let
   sources = {
     x86_64-linux = fetchurl {
       url = "https://downloads.cursor.com/production/475871d112608994deb2e3065dfb7c6b0baa0c54/linux/x64/Cursor-3.0.16-x86_64.AppImage";
-      hash = "sha256-/kBlLvaF3/7WSEI6ika6opgUVDQVo7vLVLsgG8htEpk=";
+      hash = "sha256-dN8tFSppIpO/P0Thst5uaNzlmfWZDh0Y81Lx1BuSYt0=";
       # hash = lib.fakeSha256;
     };
   };
